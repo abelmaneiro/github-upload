9a16d81d3d68e5292e6d37fad75c4c6c0f9012ad
Abel <abel@Abels-MBP.broadband>
2019-09-26 23:11:10 +0100

    initializing Repository

---
+++ forargs.scala
@@ -0,0 +1,2 @@
+for (args <- args)
+  println(args)
---
+++ helloargs.scala
@@ -0,0 +1,15 @@
+println("Hello, " + args(0) + "!")
+println("")
+var i = 0
+while (i < args.length) {
+  println(args(i))
+  i += 1
+}
+println("")
+i = 0
+while (i < args.length) {
+  print(" ")
+  print(args(i))
+  i += 1
+}
+println("")
